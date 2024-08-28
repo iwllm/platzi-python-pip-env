fba6e1e317d0c26486d1a0c2fe8bb7c7a3969dc8
William Genoy <william.genoy@gmail.com>
2024-08-28 16:10:07 -0400

    Agregando pandas y graficar un pie_chart de la poblacion mundial

--- app/main.py
+++ app/main.py
@@ -4,20 +4,16 @@ import charts
 import pandas as pd
 
 def run():
-  '''
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  '''
-
-  '''
+  
+  ## Datos mundiales
   df = pd.read_csv('data.csv')
   df = df[df['Continent'] == 'Africa']
 
   countries = df['Country'].values
   percentages = df['World Population Percentage'].values
-  charts.generate_pie_chart(countries, percentages)
-  '''
+  charts.generate_pie_chart('World_Population_Percentage', countries, percentages)
+
+  ## Datos por pais
   data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
 
